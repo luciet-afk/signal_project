b7645178f09af090d05a4be579102efd3d86eb90
luciet-afk <lucy.turkova@gmail.com>
2026-04-17 22:20:37 +0200

    Corrected style guide violations in FileOutputStrategy

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -9,26 +9,31 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class FileOutputStrategy implements OutputStrategy {
 
-    private String BaseDirectory;
+    //Changed BaseDirectory to baseDirectory per camelCase.
+    private String baseDirectory;
 
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
+    //Changed from file_map to FILE_MAP, based on final constant. Thus must abide UPPER_SNAKE_CASE.
+    //Changed public to private to stop information leakage - reinforces encapsulation.
+    private final ConcurrentHashMap<String, String> FILE_MAP = new ConcurrentHashMap<>(); 
 
     public FileOutputStrategy(String baseDirectory) {
-
-        this.BaseDirectory = baseDirectory;
+        //Changed to fit camelCase.
+        this.baseDirectory = baseDirectory;
     }
 
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            Files.createDirectories(Paths.get(baseDirectory)); //Change to baseDirectory based on camelCase
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
         // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+        //Changed to FILE_MAP to abide UPPER_SNAKE_CASE for finals. 
+        //Change to baseDirectory to abide by camelCase.
+        String FilePath = FILE_MAP.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString()); 
 
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
